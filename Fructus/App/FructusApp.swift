43fc470c6ce5b8af3d828d3655b6cfd17824b71b
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-01-16 19:56:15 -0300

    Added initial config

---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ivan Romero on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
